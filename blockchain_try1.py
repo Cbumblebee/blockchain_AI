1c794114e62e765618b8cac96db848499be61161
Cbumblebee <maja.schachreiter@gmail.com>
2023-07-13 14:15:59 +0200

    change back

--- blockchain_try1.py
+++ blockchain_try1.py
@@ -95,7 +95,7 @@ def transaction():
   # Then we let the client know it worked out
   return "Transaction submission successful\n"
 
-#~~~~~~~~ consensus change1 ~~~~~~~~#
+#~~~~~~~~ consensus ~~~~~~~~#
 
 @node.route('/blocks', methods=['GET'])
 def get_blocks():
